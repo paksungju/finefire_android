6976676667f179f59b8c6e125cf70af72ffd8406
김명년 <act1000@naver.com>
2020-01-03 14:15:15 +0900

    bbs 레이아웃 추가 설정 레이아웃 추가

--- app/src/main/java/com/finefire/finefire/fragment/BBSFragment.kt
+++ app/src/main/java/com/finefire/finefire/fragment/BBSFragment.kt
@@ -5,7 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.finefire.finefire.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import kotlinx.android.synthetic.main.fragment_bbs.*
+import kotlinx.android.synthetic.main.layout_bbs_item.view.*
+import org.json.JSONArray
+import org.json.JSONObject
 
 class BBSFragment : Fragment() {
 
@@ -17,4 +25,87 @@ class BBSFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_bbs, container, false)
     }
 
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        ll_notice.visibility = View.VISIBLE
+        ll_qna.visibility = View.GONE
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                if(tab.position == 0){
+                    ll_notice.visibility = View.VISIBLE
+                    ll_qna.visibility = View.GONE
+                }else if (tab.position == 1){
+                    ll_notice.visibility = View.GONE
+                    ll_qna.visibility = View.VISIBLE
+                }
+            }
+        })
+
+
+        var list = JSONArray()
+        var obj = JSONObject()
+        obj.put("title", "제목1")
+        obj.put("date", "2020-01-01")
+        list.put(obj)
+        obj.put("title", "제목2")
+        obj.put("date", "2020-01-03")
+        list.put(obj)
+        obj.put("title", "제목3")
+        obj.put("date", "2020-01-04")
+        list.put(obj)
+        obj.put("title", "제목4")
+        obj.put("date", "2020-01-05")
+        list.put(obj)
+
+        rv_notice.adapter = Adapter(list)
+        rv_notice.layoutManager = LinearLayoutManager(this.context)
+        rv_notice.setHasFixedSize(true)
+
+        obj.put("title", "제목6")
+        obj.put("date", "2020-01-07")
+        list.put(obj)
+        obj.put("title", "제목7")
+        obj.put("date", "2020-01-07")
+        list.put(obj)
+        rv_qna.adapter = Adapter(list)
+        rv_qna.layoutManager = LinearLayoutManager(this.context)
+        rv_qna.setHasFixedSize(true)
+
+    }
+
+    class Adapter() : RecyclerView.Adapter<Adapter.MainViewHolder>() {
+        constructor(list: JSONArray) : this() {
+            this.list = list
+        }
+
+        private var list: JSONArray = JSONArray()
+
+        override fun onCreateViewHolder(parent: ViewGroup, p1: Int) = MainViewHolder(parent)
+
+
+        override fun getItemCount(): Int {
+            return list.length()
+        }
+
+        override fun onBindViewHolder(holer: MainViewHolder, position: Int) {
+
+            list[position].let { item ->
+                with(holer) {
+                    var i = item as? JSONObject
+                    tv_title.text = i?.getString("title")
+                    tv_date.text = i?.getString("date")
+                }
+            }
+        }
+
+        inner class MainViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.layout_bbs_item, parent, false)) {
+            val tv_title = itemView.tv_title
+            val tv_date = itemView.tv_date
+        }
+    }
 }
--- app/src/main/java/com/finefire/finefire/fragment/SettingFragment.kt
+++ app/src/main/java/com/finefire/finefire/fragment/SettingFragment.kt
@@ -1,11 +1,23 @@
 package com.finefire.finefire.fragment
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.storage.StorageManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.finefire.finefire.LoginActivity
 import com.finefire.finefire.R
+import kotlinx.android.synthetic.main.fragment_setting.*
+
 
 class SettingFragment : Fragment() {
 
@@ -15,6 +27,51 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        ll_my.setOnClickListener {
+
+        }
+        ll_terms.setOnClickListener {
+            val intent = Intent(android.content.Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.finefire.co.kr/bbs/content.php?co_id=e_provision")
+            startActivity(intent)
+        }
+        ll_company.setOnClickListener {
+            val intent = Intent(android.content.Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://www.finefire.co.kr/bbs/content.php?co_id=c_about")
+            startActivity(intent)
+        }
+        ll_logout.setOnClickListener {
+            val intent = Intent(this.context, LoginActivity::class.java)
+            startActivity(intent)
+            this.activity?.finish()
+        }
+        ll_version.setOnClickListener {
+            var cm = this.context?.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            cm.setPrimaryClip(ClipData.newPlainText("text", com.finefire.finefire.util.StorageManager().getToken(this.context!!)))
+            Toast.makeText(context,"토큰이 복사되었습니다.",Toast.LENGTH_SHORT).show();
+        }
+
+        tv_version.text = getVersionInfo(this.context)
+    }
+    fun getVersionInfo(context: Context?): String? {
+        var version = "Unknown"
+        val packageInfo: PackageInfo
+        if (context == null) {
+            return version
+        }
+        try {
+            packageInfo = context.getApplicationContext()
+                .getPackageManager()
+                .getPackageInfo(context.getApplicationContext().getPackageName(), 0)
+            version = packageInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+        }
+        return version
     }
 
 }
